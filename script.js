5833acb476521ebd4a6cdd54db682fb78e2ac2ba
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-11-03 20:15:46 +0100

    Change of the approach
    Changes divs to buttons in html. CSS Nullifies basic button style.
    The most important thing happens inside java script file. Game board is now 3x3 matrix. Moreover I plan to write entire thing to be ready to play in console, then bind it with the DOM.

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 'use strict'
 
 const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3;
+    let board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push("");
+        }
+    }
+
+    const getBoard = () => board;
+
+    return { getBoard }
+})();
+
+function createPlayer (name, sign) {
+    
+    return { name, sign };
+}
+
+const oPlayer = createPlayer("oPlayer", "O");
+const xPlayer = createPlayer("xPlayer", "X");
+
+const gameFlow = (function () {
+    let activePlayer = oPlayer;
+    let board = gameBoard.getBoard();
+    function placeSign(row, column) {
+        board[row][column] = activePlayer.sign;
+        return board;
+    }
+    const updateBoard = () => console.log(gameBoard.getBoard());
+
+    return { placeSign, updateBoard }
+})();
+
+
+console.log(xPlayer.name);
+console.log(gameBoard.getBoard());
+
+
+
+
+
+/*
+const gameBoard = (function () {
     let gameArray = ["O","X","O","X","O","X","O","X","O"];
     return {gameArray};
 })();
@@ -22,9 +68,8 @@ function displayBoard() {
     });
 
 }
-
 const board = document.getElementById("gameboard");
 const boxes = document.querySelectorAll(".box");
 const boxesArr = Array.from(boxes);
 displayBoard();
-console.log(boxesArr);
+*/
